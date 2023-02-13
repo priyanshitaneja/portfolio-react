05cfb62d041a8b8e07a7b3ee0b613ed9164b07eb
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2023-02-13 19:31:18 +0530

    basic design created

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <h1>
+          coming soon...
+        </h1>
     </div>
   );
 }
