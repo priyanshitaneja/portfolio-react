b61d9fd5e6c0b53bcbce97dc78857a57a09388f7
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2023-04-27 16:42:45 +0530

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import "./App.css";
 import { ComingSoon } from "./components/ComingSoon";
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <ComingSoon />
-    </div>
+    // <div className="App">
+      
+    // </div>
+    <>
+      <Routes>
+        <Route path="/" element={<ComingSoon />} />
+        <Route path="/products" element={<h1>Hi</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </>
   );
 }
 
