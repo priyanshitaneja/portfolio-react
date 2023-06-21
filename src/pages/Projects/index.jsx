ab508d23f6796d1a38f8259277dc7c95cd3e4709
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2023-06-21 07:40:10 +0530

    projects content added

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -8,23 +8,115 @@ const Projects = () => {
     <div className="projects-grid">
       <ProjectCard
         name="MGemi"
-        description=""
+        imageUrl="https://i.imgur.com/I0CGwKO.jpg"
+        description="Optimised the website of MGemi"
         deployedUrl="https://mgemi.com/"
+      />
+      <ProjectCard
+        name="Beeya Wellness"
+        description="Optimised website for a women wellness Brand"
+        imageUrl="https://i.imgur.com/aJG9ETY.jpg"
+        deployedUrl="https://beeyawellness.com/"
+        githubUrl=""
+      />
+      <ProjectCard
+        name="Oriserve"
+        description="Developed the website of Oriserve (Wordpress)"
+        imageUrl="https://i.imgur.com/3mbLuai.png"
+        deployedUrl="https://www.oriserve.com/"
+      />
+      <ProjectCard
+        name="Aventon"
+        description="Pixel Perfected the Aventon website"
+        imageUrl="https://i.imgur.com/EBdMxB5.png"
+        deployedUrl="https://www.aventon.com/"
+        githubUrl=""
+      />
+      <ProjectCard
+        name="React Dashboard"
+        description="React Dashboard with Google OAuth Login"
+        imageUrl="https://i.imgur.com/jeOdxcT.png"
+        deployedUrl="https://react-db-black.vercel.app/"
+        githubUrl={"https://github.com/priyanshitaneja/react-db"}
+      />
+      <ProjectCard
+        name="Generator-AI"
+        description="A blog writer that writes content on a topic based on tags and tone provided"
+        imageUrl="https://i.imgur.com/TOARWBF.png"
+        deployedUrl="https://generator-ai-smoky.vercel.app/"
+        githubUrl="https://github.com/priyanshitaneja/Generator-AI"
+      />
+      <ProjectCard
+        name="Glyde Rides"
+        description="https://i.imgur.com/ETnn8zX.png?1"
+        imageUrl="https://i.imgur.com/F13ILYK.png"
+        deployedUrl="https://glyderides.com/"
+        githubUrl=""
       />
       <ProjectCard
         name="Real Time Clock"
         description="Optimised Real time Digital Clock"
+        imageUrl="https://i.imgur.com/P4nNPRc.png"
         deployedUrl="https://priyanshitaneja.github.io/realTimeClock/"
         githubUrl="https://github.com/priyanshitaneja/realTimeClock"
       />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      {/* <ProjectCard src={"https://picsum.photos/300/300/?blur"} name={"Real Time Clock"} />
-                <ProjectCard src={"https://picsum.photos/300/300/?blur"} name={"Real Time Clock"} />
-                <ProjectCard src={"https://picsum.photos/300/300/?blur"} name={"Real Time Clock"} /> */}
+      <ProjectCard
+        name="Blurry Loading"
+        description="A blurry loading webpage"
+        imageUrl="https://i.imgur.com/zOzfczB.png"
+        deployedUrl="https://priyanshitaneja.github.io/Blurry-Loading/"
+        githubUrl="https://github.com/priyanshitaneja/Blurry-Loading"
+      />
+      <ProjectCard
+        name="Javascript Carousel"
+        description="A minimal JavaScript Carousel"
+        imageUrl="https://i.imgur.com/j7uxqhV.png"
+        deployedUrl="https://priyanshitaneja.github.io/js-carousel/"
+        githubUrl="https://github.com/priyanshitaneja/js-carousel"
+      />
+      <ProjectCard
+        name="React Carousel"
+        description="A minimal React Carousel"
+        imageUrl="https://i.imgur.com/WVHISn0.png"
+        deployedUrl="https://golden-bienenstitch-8c4c8e.netlify.app/"
+        githubUrl="https://github.com/priyanshitaneja/react-carousel"
+      />
+      <ProjectCard
+        name="Compensation Projection"
+        description="Graphical representation of compensation on yearly basis"
+        imageUrl="https://i.imgur.com/hAUN1Nk.png"
+        deployedUrl="https://compensation-projection.netlify.app/"
+        githubUrl="https://github.com/priyanshitaneja/compensation-projection"
+      />
+      <ProjectCard
+        name="Keeper"
+        description="Clone of Google Keep web app"
+        imageUrl="https://i.imgur.com/aXNMlMf.png"
+        deployedUrl="https://j96po7.csb.app/"
+        githubUrl="https://github.com/priyanshitaneja/keeper"
+      />
+      <ProjectCard
+        name="To-Do List"
+        description="To-Do List"
+        imageUrl="https://i.imgur.com/IuOdymq.png"
+        deployedUrl="https://7upub7.csb.app/"
+        githubUrl="https://github.com/priyanshitaneja/todoList"
+      />
+      <ProjectCard
+        name="Simon Game"
+        description="Simon- A Memory Game"
+        imageUrl="https://i.imgur.com/w5PsY2N.png"
+        deployedUrl="https://priyanshitaneja.github.io/SimonGame/"
+        githubUrl="https://github.com/priyanshitaneja/SimonGame"
+      />
+      
+      {/* <ProjectCard
+        name=""
+        description=""
+        imageUrl=""
+        deployedUrl=""
+        githubUrl=""
+      /> */}
     </div>
   );
 };
